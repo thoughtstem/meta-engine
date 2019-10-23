a0e855d2af5f957660b4fdbfb2a275bc9949d7ea
aBlender <JasonLongLe@gmail.com>
2019-10-23 10:58:04 -0700

    changed on-rule to a syntax rule

--- meta-engine/extensions/common-components/on-rule.rkt
+++ meta-engine/extensions/common-components/on-rule.rkt
@@ -4,8 +4,13 @@
 
 (require "../../core/main.rkt")
 
-(define (on-rule condition updater [else (get-value (CURRENT-COMPONENT))])
-  (if condition
-      updater
-      else
-      ))
+;(define-syntax-rule (on-rule condition updater [else (get-value (CURRENT-COMPONENT))])
+;  (if condition
+;      updater
+;      else
+;      ))
+
+(define-syntax on-rule
+  (syntax-rules ()
+    [(on-rule condition updater else) (if condition updater else)]
+    [(on-rule condition updater) (on-rule condition updater (get-value (CURRENT-COMPONENT)))]))
