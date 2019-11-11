5c03c801773c83520b144cec1a65f52abdffa72f
aBlender <JasonLongLe@gmail.com>
2019-11-11 12:30:43 -0800

    Added get-mouse-position and on-sprite-click

--- meta-engine/extensions/input/mouse-manager.rkt
+++ meta-engine/extensions/input/mouse-manager.rkt
@@ -4,12 +4,15 @@
          mouse-change-down?
          on-mouse
          on-mouse-hold
-         mouse-manager-entity)
+         mouse-manager-entity
+         get-mouse-position
+         on-sprite-click)
 
 (require "../../core/main.rkt")
 
 (require 
   "../rendering/renderer.rkt"
+  "../rendering/animated-sprite.rkt"
   "../common-components/main.rkt")
 
 (define-component mouse-hash hash?)
@@ -43,3 +46,30 @@
           (last-mouse-hash #f (get-mouse-hash))
           (mouse-hash mouse-input (hash-copy mouse-input))
           ))
+
+(define (get-mouse-position [e (CURRENT-ENTITY)])
+  (define g (CURRENT-GAME))
+  (define mouse-hash (get-mouse-hash (get-entity g (has-name 'mouse-manager))))
+  (hash-ref mouse-hash 'position))
+
+(define (on-sprite-click updater [else (get-value (CURRENT-COMPONENT))])
+    (define pos (get-position))
+    (define m-pos (get-mouse-position))
+  
+    (define x (posn-x pos))
+    (define y (posn-y pos))
+    (define mx (posn-x m-pos))
+    (define my (posn-y m-pos))
+    (define w (get-entity-width))
+    (define h (get-entity-height))
+  
+    (define touching-pointer?
+      (and (> mx (- x (/ w 2)))
+           (< mx (+ x (/ w 2)))
+           (> my (- y (/ h 2)))
+           (< my (+ y (/ h 2)))))
+  (on-mouse 'left
+            (if touching-pointer?
+                updater
+                else)
+            else))
